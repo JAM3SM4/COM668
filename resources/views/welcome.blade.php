ece7033666908863d861e56c8d4746a64db86521
unknown <jamesm4@outlook.com>
2020-09-30 11:01:25 +0100

    Update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Formula E-Bet - Where e-Sports betting comes to life</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Formula E-Bet
                 </div>
 
                 <div class="links">
