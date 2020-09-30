643bd09d02f7eaa0c05f94ad06cdc9d68bcd182a
unknown <jamesm4@outlook.com>
2020-09-30 20:34:48 +0100

    Evening commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Formula E-Bet - Where e-Sports betting comes to life</title>
+        <title>Formula E-Bet - Esports Betting</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
