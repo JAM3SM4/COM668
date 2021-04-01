16fb7e884882fb2eaeedf090a8060da9ac4b9421
unknown <jamesm4@outlook.com>
2021-04-01 07:25:44 +0100

    Begin to write CSS, Javascript commit

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -12,34 +12,41 @@ Released   : 20140225
 -->
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title></title>
-<meta name="keywords" content="" />
-<meta name="description" content="" />
-<link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900" rel="stylesheet" />
-<link href="css/default.css" rel="stylesheet"/>
-<link href="css/fonts.css" rel="stylesheet"/>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title></title>
+	<meta name="keywords" content="" />
+	<meta name="description" content="" />
+	<link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900" rel="stylesheet" />
+
+	<link href="css/default.css" rel="stylesheet"/>
+	<link href="css/fonts.css" rel="stylesheet"/>
 
 </head>
 <body>
 <div id="header-wrapper">
 	<div id="header" class="container">
 		<div id="logo">
-			<h1><a href="#">SimpleWork</a></h1>
+			<h1><a href="/">Formula Extreme</a></h1>
 		</div>
 		<div id="menu">
 			<ul>
-				<li class="current_page_item"><a href="/" accesskey="1" title="">Homepage</a></li>
-                <li><a href="/aboutus" accesskey="3" title="">About Us</a></li>
-                <li><a href="/contactus" accesskey="2" title="">Contact Us</a></li>
-				<li><a href="/moreinfo" accesskey="4" title="">More Information</a></li>
-                <li><a href="/terms" accesskey="5" title="">T&Cs</a></li>
-                <li><a href="/events" accesskey="2" title="">Events</a></li>
+				<li class="{{ Request::path() === '/' ? 'current_page_item' : ''}}"><a href="/" accesskey="1" title="">Homepage</a></li>
+                <li class="{{ Request::path() === 'aboutus' ? 'current_page_item' : ''}}"><a href="/aboutus" accesskey="3" title="">About Us</a></li>
+                <li class="{{ Request::path() === 'contactus' ? 'current_page_item' : ''}}"><a href="/contactus" accesskey="2" title="">Contact Us</a></li>
+				<li class="{{ Request::path() === 'moreinfo' ? 'current_page_item' : ''}}"><a href="/moreinfo" accesskey="4" title="">More Information</a></li>
+                <li class="{{ Request::path() === 'terms' ? 'current_page_item' : ''}}"><a href="/terms" accesskey="5" title="">T&Cs</a></li>
+                <li class="{{ Request::path() === 'events' ? 'current_page_item' : ''}}"><a href="/events" accesskey="2" title="">Events</a></li>
                 
 			</ul>
 		</div>
 	</div>
+
+		@yield ('header')
+	</div>
+
+	@yield ('content')
+	<div id="copyright" class="container">
+	<p>&copy; Untitled. All rights reserved. | Photos by <a href="http://fotogrph.com/">Fotogrph</a> | Design by <a href="http://templated.co" rel="nofollow">TEMPLATED</a>.</p>
 </div>
-@yield ('content')
 </body>
 </html>
